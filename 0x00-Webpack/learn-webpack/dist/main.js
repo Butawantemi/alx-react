549d16aa092e4c76ce4c6a5fbb938f1afa78aad1
Butawantemi <japhetbuta@gmail.com>
2024-10-11 15:00:33 +0300

    learn webpack

--- 0x00-Webpack/learn-webpack/dist/main.js
+++ 0x00-Webpack/learn-webpack/dist/main.js
@@ -26,7 +26,7 @@ eval("console.log(\"Hello!, Webpack\")\n\n//# sourceURL=webpack://learn-webpack/
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	__webpack_modules__["./src/app.js"]();
 /******/ 	
 /******/ })()
 ;
