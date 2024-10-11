dfb8e9fb7a72e2064119cd78eb1752eb29e9409b
Butawantemi <japhetbuta@gmail.com>
2024-10-11 18:17:02 +0300

    learn webpack

--- 0x00-Webpack/learn-webpack/webpack.config.js
+++ 0x00-Webpack/learn-webpack/webpack.config.js
@@ -13,30 +13,23 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'deploy')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presents: ['@babel/present-env']
+                    }
+                }
+            }
+        ]
     }
 }
 
-module = {
-    rules: [
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presents: ['@babel/present-env']
-                }
-            }
-        }
-    ]
-}
-
-module = {
-    rules: [
-        ...
-        {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader']
-        }
-    ]
-}
+module: {
+    
+  }
