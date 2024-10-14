7c12538872b7aec0af38e287d18ac10e6b903c7b
Butawantemi <japhetbuta@gmail.com>
2024-10-14 09:55:21 +0300

    learn webpack

--- 0x00-Webpack/learn-webpack/webpack.config.js
+++ 0x00-Webpack/learn-webpack/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
         path: path.resolve(__dirname, 'deploy'),
     },
     devServer: {
-        contentBase: './deploy',
+        contentBase: path.join(__dirname, './deploy'),
         open: true,
         port: 8080
     },
