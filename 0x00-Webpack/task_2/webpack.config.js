2203e89dfb5cf9444b54c6fa2e9924724de470b1
Butawantemi <japhetbuta@gmail.com>
2024-10-15 10:57:17 +0300

    Adding CSS & Images

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource'
       }
     ]
   }
