20cd2bdaac4536acd8ee967506c5625bb4017870
Butawantemi <japhetbuta@gmail.com>
2024-10-15 09:33:08 +0300

    Learning how to use Webpack with a config file

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -3,4 +3,4 @@ import $ from 'jquery';
 $(body).append('<p>Holberton Dashboard</p>')
 $(body).append('<p>Dashboard data for the students</p>')
 $(body).append('<button>Click here to get started</button>')
-$(body).append()
+$(body).append('<p id="count"></p>')
